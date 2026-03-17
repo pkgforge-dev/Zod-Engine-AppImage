0a131cbd0305c91349a2369e8b25dc4fb2f47acd
Link4Electronics <link4electronics@gmail.com>
2026-03-17 09:13:56 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,6 +7,7 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
+    gcc-libs     \
     libdecor     \
     sdl12-compat \
     sdl_image    \
