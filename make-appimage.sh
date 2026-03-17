ed61dba5082f4da665dd826deca371b4f06cbcf8
Link4Electronics <link4electronics@gmail.com>
2026-03-17 05:58:35 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun ./AppDir/bin/zod_launcher ./AppDir/bin/zod
 
 # Additional changes can be done in between here
 
