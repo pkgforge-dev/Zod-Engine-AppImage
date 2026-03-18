ada6bfd2782fb3fa57f91ef5976102ed769993f0
Link4Electronics <link4electronics@gmail.com>
2026-03-18 14:39:30 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -25,7 +25,7 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package mysql
+#make-aur-package
 
 # If the application needs to be manually built that has to be done down here
 echo "Getting Zod Engine binary..."
