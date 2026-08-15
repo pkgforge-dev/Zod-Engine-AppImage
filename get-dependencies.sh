f320e5279b055a7c3f69f753105fde074b52a366
Link4Electronics <link4electronics@gmail.com>
2026-08-15 05:46:00 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -22,10 +22,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package
-
-# If the application needs to be manually built that has to be done down here
 echo "Getting Zod Engine binary..."
 echo "---------------------------------------------------------------"
 VERSION=2011-09-06
