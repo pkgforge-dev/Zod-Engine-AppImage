7b41788cd4438c21b32ccfe807c9f8315b2a7b1d
Link4Electronics <link4electronics@gmail.com>
2026-08-29 11:09:55 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,7 +9,7 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export DEPLOY_GTK=1
 export GTK_DIR=gtk-3.0
-export DEPLOY_OPENGL=1
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/zod_launcher ./AppDir/bin/zod
